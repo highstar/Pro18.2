a362ab2fed416b4652ebf977d7899f880500c7c7
高星 <gaoxing@gaos-macbook-pro.local>
2018-06-04 15:25:15 +0800

    Make changes to one collection but not to its copy.

--- Pro18.2/main.m
+++ Pro18.2/main.m
@@ -10,8 +10,34 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableArray *dataArray = [NSMutableArray arrayWithObjects:
+                                     [NSMutableString stringWithString:@"one"],
+                                     [NSMutableString stringWithString:@"two"],
+                                     [NSMutableString stringWithString:@"three"],
+                                     nil
+                                     ];
+        NSMutableArray *dataArray2;
+        NSMutableString *mStr;
+        
+        NSLog(@"dataArray: ");
+        for (NSString *elem in dataArray)
+            NSLog(@" %@", elem);
+        
+        // make a copy, then change one of the strings
+        
+        dataArray2 = [dataArray mutableCopy];
+        
+        mStr = [NSMutableString stringWithString:dataArray2[0]];
+        [mStr appendString:@"ONE"];
+        [dataArray2 replaceObjectAtIndex:0 withObject:mStr];
+        
+        NSLog (@"dataArray: ");
+        for (NSString *elem in dataArray)
+            NSLog(@" %@", elem);
+        
+        NSLog (@"dataArray2: ");
+        for (NSString *elem in dataArray2)
+            NSLog(@" %@", elem);
     }
     return 0;
 }
